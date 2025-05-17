e51dd5e295dede28ef24bca5208b06cb7fc2e890
jakuninoleg <oleg.kemz@gmail.com>
2025-05-18 00:16:40 +0300

    config change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,21 +8,20 @@ export default defineNuxtConfig({
       lang: 'ru',
       start_url: '/',
       display: 'standalone',
-      srcDir: 'public',
-      filename: 'sw.ts',
-      strategies: 'injectManifest',
       background_color: '#ffffff',
-      theme_color: '#3b82f6',
+      theme_color: '#C3A8F1',
       icons: [
         {
           src: '/icon-192.png',
           sizes: '192x192',
           type: 'image/png',
+          purpose: 'any maskable'
         },
         {
           src: '/icon-512.png',
           sizes: '512x512',
           type: 'image/png',
+          purpose: 'any maskable'
         },
       ],
     },
