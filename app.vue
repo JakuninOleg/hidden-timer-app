39f4fafe87185ad155588efd62b5d5044c7f8571
jakuninoleg <oleg.kemz@gmail.com>
2025-05-18 01:22:57 +0300

    added padding

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <VitePwaManifest />
-  <div class="bg-stopwatchPurple min-h-screen flex items-center justify-center">
-    <NuxtPage />
+  <div class="bg-stopwatchPurple min-h-screen flex items-center justify-center px-6">
+    <NuxtPage/>
   </div>
 </template>
 
